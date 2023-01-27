ff7907c676f5ce2a00070df3a4c3b6d2d4b865cc
PulkitBxtra <PulkitBxtra@users.noreply.github.com>
2023-01-27 21:53:01 +0530

    fixed auth

--- server.js
+++ server.js
@@ -1,49 +1,88 @@
-const express=require("express");
-const app=express();
-const bcrypt = require("bcrypt");
-var bodyParser = require('body-parser')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
-app.set('view engine', 'ejs');
+const express = require('express')
+const app = express()
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
 
-//had to include this line in order for body parser to work
-// app.use(express.json());
-app.use(express.urlencoded({extended:false}));
-// app.use(bodyparser.json());
+const initializePassport = require('./passport-config')
+initializePassport(
+  passport,
+  email => users.find(user => user.email === email),
+  id => users.find(user => user.id === id)
+)
 
-const users=[];
+const users = []
 
-app.get('/',(req,res)=>{
-    res.render('index.ejs',{name:'pulkit'});
+app.set('view-engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(flash())
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
+
+app.get('/', checkAuthenticated, (req, res) => {
+  res.render('index.ejs', { name: req.user.name })
 })
 
-app.get('/register',(req,res)=>{
-    res.render('register.ejs')
+app.get('/login', checkNotAuthenticated, (req, res) => {
+  res.render('login.ejs')
 })
 
-app.post('/register', async (req, res) => {
-    try {
-      const hashedPassword = await bcrypt.hash(req.body.password, 10);
-      users.push({
-        id: Date.now().toString(),
-        name: req.body.name,
-        email: req.body.email,
-        password: hashedPassword
-      })
-      res.redirect('/login')
-    } catch {
-      res.redirect('/register')
-    }
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
+}))
+
+app.get('/register', checkNotAuthenticated, (req, res) => {
+  res.render('register.ejs')
+})
+
+app.post('/register', checkNotAuthenticated, async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword
+    })
     console.log(users);
-  })
-
-
-app.post('/login',(req,res)=>{
-    
+    res.redirect('/login')
+  } catch {
+    res.redirect('/register')
+  }
 })
 
-app.get('/login',(req,res)=>{
-    res.render('login.ejs')
+app.delete('/logout', (req, res) => {
+  req.logOut()
+  res.redirect('/login')
 })
 
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  }
 
-app.listen(3000);
+  res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/')
+  }
+  next()
+}
+
+app.listen(3000)
